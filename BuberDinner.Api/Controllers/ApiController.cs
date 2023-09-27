32d3b60b0c90b8aef115f444cf926ce35497f51e
Omar Torres <jtorres@axsistec.com>
2023-09-26 23:05:07 -0600

    better response for validation errors

--- BuberDinner.Api/Controllers/ApiController.cs
+++ BuberDinner.Api/Controllers/ApiController.cs
@@ -1,18 +1,32 @@
 using BuberDinner.Api.Commons.Http;
 using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace BuberDinner.Api.Controllers;
 
 [ApiController]
-public class ApiController: ControllerBase
+public class ApiController : ControllerBase
 {
     protected IActionResult Problem(List<Error> errors)
     {
+        if (errors.Count is 0)
+        {
+            return Problem();
+        }
+        if (errors.All(error => error.Type == ErrorType.Validation))
+        {
+            return ValidationProblem(errors);
+        }
         HttpContext.Items[HttpContextItemsKey.Errors] = errors;
         var firstError = errors[0];
 
-        var statusCode = firstError.Type switch
+        return Problem(firstError);
+    }
+
+    private IActionResult Problem(Error error)
+    {
+        var statusCode = error.Type switch
         {
             ErrorType.NotFound => 404,
             ErrorType.Conflict => 409,
@@ -20,6 +34,19 @@ public class ApiController: ControllerBase
             _ => 500
         };
 
-        return Problem(statusCode: statusCode, title: firstError.Description);
+        return Problem(statusCode: statusCode, title: error.Description);
+    }
+
+    private IActionResult ValidationProblem(List<Error> errors)
+    {
+        var modelStateDictionary = new ModelStateDictionary();
+
+        foreach (var error in errors)
+        {
+            modelStateDictionary.AddModelError(
+                error.Code,
+                error.Description);
+        }
+        return ValidationProblem(modelStateDictionary);
     }
 }
